5c5c75be74ac61f3b7f1e683e5c3e2558cf8c172
syauqi <ahmad@mamiteam.com>
2021-03-29 15:30:56 +0700

    new init

---
+++ MamiAddSumLib/MamiAddSubLib.swift
@@ -0,0 +1,14 @@
+//
+//  MamiAddSubLib.swift
+//  MamiAddSumLib
+//
+//  Created by Ahmad Syauqi Albana on 29/03/21.
+//
+
+import Foundation
+
+public final class MamiAddSubLib {
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ MamiAddSumLib/MamiAddSumLib.h
@@ -0,0 +1,18 @@
+//
+//  MamiAddSumLib.h
+//  MamiAddSumLib
+//
+//  Created by Ahmad Syauqi Albana on 29/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MamiAddSumLib.
+FOUNDATION_EXPORT double MamiAddSumLibVersionNumber;
+
+//! Project version string for MamiAddSumLib.
+FOUNDATION_EXPORT const unsigned char MamiAddSumLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MamiAddSumLib/PublicHeader.h>
+
+
